01b568ee95eb1b35dd1f524945b147fff0b387c1
Maxim <malex81@yandex.ru>
2024-06-07 19:34:02 +0300

    add GrpcReview

---
+++ GrpsReview/GrpcReview.Client/Program.cs
@@ -0,0 +1,43 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpsReview.Client;
+
+// создаем канал для обмена сообщениями с сервером
+// параметр - адрес сервера gRPC
+using var channel = GrpcChannel.ForAddress("http://localhost:5135");
+await TestStreem(channel);
+//await TestGeeter(channel);
+
+static async Task TestGeeter(GrpcChannel channel)
+{
+	// создаем клиент
+	var client = new Greeter.GreeterClient(channel);
+	Console.Write("Введите имя: ");
+	string? name = Console.ReadLine();
+	// обмениваемся сообщениями с сервером
+	var reply = await client.SayHelloAsync(new HelloRequest { Name = name });
+	Console.WriteLine($"Ответ сервера: {reply.Message}");
+	Console.ReadKey();
+}
+
+static async Task TestStreem(GrpcChannel channel)
+{
+	var client = new Messenger.MessengerClient(channel);
+
+	// посылаем пустое сообщение и получаем набор сообщений
+	var serverData = client.ServerDataStream(new Request());
+
+	// получаем поток сервера
+	var responseStream = serverData.ResponseStream;
+
+	while (await responseStream.MoveNext(new CancellationToken()))
+	{
+		Response response = responseStream.Current;
+		Console.WriteLine($"{DateTime.Now:ss.ffff}: {response.Content}");
+	}
+
+	//await foreach (var response in responseStream.ReadAllAsync())
+	//{
+	//	Console.WriteLine(response.Content);
+	//}
+}
---
+++ GrpsReview/GrpcReview.Server/Services/MessengerService.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Core;
+
+namespace GrpsReview.Services;
+// source: https://metanit.com/sharp/grpc/2.5.php
+public class MessengerService : Messenger.MessengerBase
+{
+	private readonly string[] messages = { "Привет", "Как дела?", "Че молчишь?", "Ты че, спишь?", "Ну пока" };
+	public override async Task ServerDataStream(Request request,
+		IServerStreamWriter<Response> responseStream,
+		ServerCallContext context)
+	{
+		int num = 1;
+		foreach (var message in messages)
+		{
+			await responseStream.WriteAsync(new Response { Content = (num++).ToString() });
+			await responseStream.WriteAsync(new Response { Content = message });
+			// для имитации работы делаем задержку в 1 секунду
+			await Task.Delay(TimeSpan.FromSeconds(1));
+		}
+	}
+}
