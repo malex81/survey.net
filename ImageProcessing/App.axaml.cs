273b7090369d104f9cdfb498b4bfda54dfa085ed
Maxim <malex81@yandex.ru>
2024-06-23 14:51:16 +0300

    1

--- ImageProcessing/App.axaml.cs
+++ ImageProcessing/App.axaml.cs
@@ -1,26 +1,41 @@
 using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using ImageProcessing.Engine;
+using System;
 
 namespace ImageProcessing;
 
 public partial class App : Application
 {
-    private readonly EngineKernel kernel = new();
+    private EngineKernel? kernel;
 
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
     }
 
-    public override void OnFrameworkInitializationCompleted()
-    {
-        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
-        {
-            desktop.MainWindow = new MainWindow();
-        }
+	public override void OnFrameworkInitializationCompleted()
+	{
+		if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+		{
+			desktop.Exit += AppExit;
+			desktop.MainWindow = BuildMainWindow();
+		}
 
-        base.OnFrameworkInitializationCompleted();
-    }
+		base.OnFrameworkInitializationCompleted();
+	}
+
+	private void AppExit(object? sender, ControlledApplicationLifetimeExitEventArgs e)
+	{
+		kernel?.Dispose();
+	}
+
+	static Window BuildMainWindow()
+	{
+		
+
+		return new MainWindow();
+	}
 }
--- ImageProcessing/Engine/EngineKernel.cs
+++ ImageProcessing/Engine/EngineKernel.cs
@@ -6,32 +6,56 @@ using System.IO;
 using System.Reflection;
 
 namespace ImageProcessing.Engine;
-public class EngineKernel : IDisposable
+public class EngineKernel(ServiceProvider services, IConfigurationRoot config) : IDisposable
 {
-	private readonly ServiceProvider services;
-
-	public EngineKernel()
+	public class Builder(IConfigurationRoot config)
 	{
-		AppConfig = BuildAppConfig();
-		var serviceCollection = new ServiceCollection();
-		//ConfigureLogging
-		serviceCollection.AddSingleton<MainWindowModel>();
+		private readonly IConfigurationRoot config = config;
+		private readonly ServiceCollection serviceCollection = new();
 
-		services = serviceCollection.BuildServiceProvider();
+		public Builder ConfigureServices(Action<IServiceCollection, IConfigurationRoot> configure)
+		{
+			//serviceCollection.AddSingleton<MainWindowModel>();
+			//services = serviceCollection.BuildServiceProvider();
+			configure(serviceCollection, config);
+			return this;
+		}
+
+		public EngineKernel Build() => new(serviceCollection.BuildServiceProvider(), config);
 	}
+
+	public static Builder Configure(Action<ConfigurationBuilder> configure)
+	{
+		ConfigurationBuilder configBuilder = new();
+		configure(configBuilder);
+		return new(configBuilder.Build());
+	}
+
+	private readonly ServiceProvider services = services;
 
 	public IServiceProvider Services => services;
-	public IConfigurationRoot AppConfig { get; }
+	public IConfigurationRoot AppConfig { get; } = config;
 
-	static IConfigurationRoot BuildAppConfig()
+	public void Dispose()
 	{
-		var binPath = Path.GetDirectoryName(Assembly.GetEntryAssembly()!.Location);
-		var config = new ConfigurationBuilder();
-		config.SetBasePath(Path.Combine(binPath!, "../config"))
-					.AddJsonFile("appsettings.json", optional: false)
-					.AddCommandLine(Environment.GetCommandLineArgs());
-		return config.Build();
+		services.Dispose();
+		GC.SuppressFinalize(this);
 	}
+}
+
+public static class EngineKernelBuilder
+{
+	
+
+	//static IConfigurationRoot BuildAppConfig()
+	//{
+	//	var binPath = Path.GetDirectoryName(Assembly.GetEntryAssembly()!.Location);
+	//	var config = new ConfigurationBuilder();
+	//	config.SetBasePath(Path.Combine(binPath!, "../config"))
+	//				.AddJsonFile("appsettings.json", optional: false)
+	//				.AddCommandLine(Environment.GetCommandLineArgs());
+	//	return config.Build();
+	//}
 
 	static ServiceCollection ConfigureLogging(ServiceCollection services, IConfigurationRoot appConfig)
 	{
@@ -47,11 +71,5 @@ public class EngineKernel : IDisposable
 				logging.AddFile(serilogConfig);
 		});
 		return services;
-	}
-
-	public void Dispose()
-	{
-		services.Dispose();
-		GC.SuppressFinalize(this);
 	}
 }
