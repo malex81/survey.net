9b66bfe189ad7bd3c491f002badcbb88ae9f1e96
Maxim <malex81@yandex.ru>
2024-07-16 00:42:45 +0300

    add selectedImage

--- ImageProcessing/SurveyImageSmooth/ViewModels/ImageSetModel.cs
+++ ImageProcessing/SurveyImageSmooth/ViewModels/ImageSetModel.cs
@@ -1,22 +1,21 @@
 ﻿using Avalonia.Media;
 using Avalonia.Media.Imaging;
+using CommunityToolkit.Mvvm.ComponentModel;
 using ImageProcessing.Helpers;
 using ImageProcessing.SurveyImageSmooth.Config;
 using Microsoft.Extensions.Options;
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reactive.Linq;
 
 namespace ImageProcessing.SurveyImageSmooth.ViewModels;
 
 record ImageItem(string Title, IImage ImageSource);
 
-internal class ImageSetModel
+internal partial class ImageSetModel : ObservableObject
 {
 	public static readonly ImageSetModel DesignModel = new("./##/image samples");
-
 	static IEnumerable<ImageItem> LoadImages(string dirPath)
 	{
 		foreach (var filePath in Directory.EnumerateFiles(dirPath))
@@ -32,12 +31,16 @@ internal class ImageSetModel
 		}
 	}
 
+	[ObservableProperty]
+	private ImageItem? selectedImage;
+
 	public ImageItem[] Images { get; }
 
 	ImageSetModel(string relPath)
 	{
 		var samplesPath = PathHelper.FindDirectory(relPath);
 		Images = samplesPath == null ? [] : LoadImages(samplesPath).ToArray();
+		SelectedImage = Images.FirstOrDefault();
 	}
 
 	public ImageSetModel(IOptions<ImagesOptions> imageOptions) : this(imageOptions.Value.SamplesPath ?? "")
