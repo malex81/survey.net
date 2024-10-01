c9df0debbbbb66f7a6e1f1322897e2ab97f2293b
malex <malex81@yandex.ru>
2024-10-01 14:36:09 +0300

    update Avalonia to 11.1.3 version. Add publish-linux.bat

---
+++ ImageProcessing/publish-linux.bat
@@ -0,0 +1,8 @@
+@set progName=ImageProcessing
+@set output=.\output\%progName%_linux
+@rd /S /Q %output%
+
+dotnet publish "%progName%.csproj" --framework net8.0 --runtime linux-x64 -c debug --self-contained true -o "%output%\bin" /p:NoBuild=false
+xcopy /Y /E "config" %output%\config\
+
+@if not "%1" == "nopause" @pause
