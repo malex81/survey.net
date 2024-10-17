be92aa7e3e370ba54b11e7b51b51c97f5136326b
malex <malex81@yandex.ru>
2024-10-17 19:08:13 +0300

    Add ParserSurvey

---
+++ ParserSurvey/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ParserSurvey;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		//Test1.RunDocumantationExample();
+		Test1.RunSimpleTest();
+	}
+}
---
+++ ParserSurvey/Test1.cs
@@ -0,0 +1,37 @@
+﻿using HtmlAgilityPack;
+
+namespace ParserSurvey;
+static class Test1
+{
+	// https://html-agility-pack.net/from-string
+	public static void RunDocumantationExample()
+	{
+		var html =
+		@"<!DOCTYPE html>
+<html>
+<body>
+	<h1>This is <b>bold</b> heading</h1>
+	<p>This is <u>underlined</u> paragraph</p>
+	<h2>This is <i>italic</i> heading</h2>
+</body>
+</html>";
+
+		var htmlDoc = new HtmlDocument();
+		htmlDoc.LoadHtml(html);
+
+		var htmlBody = htmlDoc.DocumentNode.SelectSingleNode("//body");
+
+		Console.WriteLine(htmlBody.OuterHtml);
+	}
+
+	public static void RunSimpleTest()
+	{
+		var html = @"Привет, Вася!
+<br>Как <small>твоя жалкая жизнь</small>?
+<br>Не хочешь <custom-widget fuck-off='ушел в себя' fuck-in='Вернусь не скоро'/> повеситься?";
+		var htmlDoc = new HtmlDocument();
+		htmlDoc.LoadHtml(html);
+
+		Console.WriteLine(htmlDoc.DocumentNode.OuterHtml);
+	}
+}
